9685aa9a7e4d42c99ce5e5db3ab6a378b30f3fe3
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-01-14 12:08:09 +0100

    adding 4th par to sol

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='IHSetShoreFor',
-    version='1.7.6',
+    version='1.7.7',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
