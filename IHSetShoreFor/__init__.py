55ad2adc33295d1b8f5e787522ebc41d20768503
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-02-10 10:32:08 +0100

    deps

--- IHSetShoreFor/__init__.py
+++ IHSetShoreFor/__init__.py
@@ -2,6 +2,5 @@
 
 # Import modules and functions from your package here
 from .shoreFor import shoreFor
-from .calibration import cal_ShoreFor
 from .calibration_2 import cal_ShoreFor_2
 from .direct_run import ShoreFor_run
