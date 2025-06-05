85c182a4ccb11379c9c4e3b19c474923e2d0b897
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-06-05 10:48:23 +0200

    Correcting writer bug

--- IHSetShoreFor/__init__.py
+++ IHSetShoreFor/__init__.py
@@ -1,6 +1,6 @@
 # src/__init__.py
 
 # Import modules and functions from your package here
-from .shoreFor import shoreFor
+from .shoreFor import shoreFor_Yini, shoreFor_idx_Yini
 from .calibration_2 import cal_ShoreFor_2
 from .direct_run import ShoreFor_run
