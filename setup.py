3adc344a9aa4575f67b35ca583567bc12a2b9d31
Lucas de Freitas Pereira <44470022+defreitasL@users.noreply.github.com>
2025-12-02 20:14:07 +0100

    Adding hybrid

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='IHSetShoreFor',
-    version='1.9.12',
+    version='1.9.13',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
